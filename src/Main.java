9aa3ec86ff3691dfec8b37d5115258a14236e2ec
sskotyarus <ss.kovalevskiy@yandex.ru>
2017-04-07 18:08:58 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World from Intellij Idea!");
         System.out.println("I'm very proud of myself!");
+//        New commit
+        System.out.println("One more line");
     }
 }
