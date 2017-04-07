3bf17c9dac1d72452957e0121021efd02553e760
sskotyarus <ss.kovalevskiy@yandex.ru>
2017-04-07 18:17:53 +0300

    forth commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("One more difference");
         System.out.println("Hello World from Intellij Idea!");
         System.out.println("I'm very proud of myself!");
 //        New commit
